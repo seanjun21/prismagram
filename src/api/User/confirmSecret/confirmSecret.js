a0268b6d0ad3eaea64be3065afdd025185a8fe9d
Sean Jun <seanjun21@gmail.com>
2019-06-23 19:09:00 -0700

    follow unfollow resolver

--- src/api/User/confirmSecret/confirmSecret.js
+++ src/api/User/confirmSecret/confirmSecret.js
@@ -7,6 +7,12 @@ export default {
       const { email, secret } = args;
       const user = await prisma.user({ email });
       if (user.loginSecret === secret) {
+        await prisma.updateUser({
+          where: { id: user.id },
+          data: {
+            loginSecret: '',
+          },
+        });
         return generateToken(user.id);
       }
       throw Error('Wrong email/secret combination');
